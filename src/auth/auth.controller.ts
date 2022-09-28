cb61f12c45dbd9392393b31301cc78d272c689dc
Dreezy <bankoleidris@gmail.com>
2022-09-28 13:42:38 +0000

    authentication, sign up, login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Controller('api/v1/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('signup')
-  signUp() {
-    return this.authService.signUp();
+  signUp(@Body() dto: CreateUserDto) {
+    return this.authService.signUp(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
-  login() {
-    return this.authService.login();
+  login(@Body() dto: LoginUserDto) {
+    return this.authService.login(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime';
+import * as argon from 'argon2';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -11,11 +17,41 @@ export class AuthService {
     private configService: ConfigService,
   ) {}
 
-  signUp() {
-    return 'Acc created successfully';
+  async signUp(dto: CreateUserDto) {
+    await this.prisma.$connect();
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+          email: dto.email,
+          role: dto.role,
+          hash: hash,
+          country: dto.country,
+        },
+      });
+      return {
+        data: user,
+        message: 'Account created successfully',
+        success: true,
+      };
+    } catch (error) {
+      console.log(error);
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new ForbiddenException(
+          'User with these credentials already exists',
+        );
+      } else if (error instanceof PrismaClientValidationError) {
+        throw new ForbiddenException('');
+      } else {
+        return error;
+      }
+    }
   }
 
-  login() {
+  login(dto: LoginUserDto) {
+    console.log(dto);
     return 'user logged in successfully';
   }
 }
