79683740acf68a1cf739e56873fe0032a247bb21
Dreezy <bankoleidris@gmail.com>
2022-09-28 10:32:00 +0000

    authentication, sign up, login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  signUp() {
+    return 'Acc created successfully';
+  }
+
+  login() {
+    return 'user logged in successfully';
+  }
+}
